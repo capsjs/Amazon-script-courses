0ac7d6c495d003852f714df9004181ab7afd112d
manon-aut <audibertmanon@hotmail.com>
2024-04-18 10:47:29 +0200

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,15 +1,12 @@
-import { cart, RemoveFromCart } from "../data/cart.js";
+import { cart, RemoveFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
-import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions } from '../data/deliveryOptions.js';
 
-hello();
-
 const today = dayjs();
 const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+//console.log(deliveryDate.format('dddd, MMMM D'));
 
 let cartSummaryHTML = '';
 
@@ -35,13 +32,14 @@ cart.forEach((cartItem) => {
   });
 
   const today = dayjs();
-  const deliveryDate = today.add(
-    deliveryOption.deliveryDays,
-    'days'
-  );
-  const dateString = deliveryDate.format(
-    'dddd, MMMM D'
-  );
+    const deliveryDate = today.add(
+      deliveryOption.deliveryDays,
+       'days'
+      );
+    const dateString = deliveryDate.format(
+      'dddd, MMMM D'
+    );
+
 
 
   cartSummaryHTML +=
@@ -52,7 +50,6 @@ cart.forEach((cartItem) => {
 <div class="delivery-date">
   Delivery date: ${dateString}
 </div>
-
 <div class="cart-item-details-grid">
   <img class="product-image"
     src="${matchingProduct.image}">
@@ -88,20 +85,19 @@ cart.forEach((cartItem) => {
 });
 
 function deliveryOptionsHTML(matchingProduct, cartItem) {
-
   let html = '';
 
   deliveryOptions.forEach((deliveryOption) => {
     const today = dayjs();
     const deliveryDate = today.add(
       deliveryOption.deliveryDays,
-      'days'
-    );
+       'days'
+      );
     const dateString = deliveryDate.format(
       'dddd, MMMM D'
     );
 
-    const priceSting = deliveryOption.priceCents === 0
+    const priceString = deliveryOption.priceCents === 0
       ? 'FREE'
       : `$${formatCurrency(deliveryOption.priceCents)} - `;
 
@@ -109,7 +105,8 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     html +=
       `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}"
+    data-delivery-option-id="${deliveryOption.id}">
     <input type="radio"
     ${isChecked ? 'checked' : '' }
       class="delivery-option-input"
@@ -119,7 +116,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         ${dateString}
       </div>
       <div class="delivery-option-price">
-        ${priceSting} Shipping
+        ${priceString} Shipping
       </div>
     </div>
   </div>
@@ -146,5 +143,13 @@ document.querySelectorAll('.js-delete-link')
       //console.log(container);
       container.remove();
     });
-  })
+  });
+
+  document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+      element.addEventListener('click', () => {
+        const {productId, deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
+      });
+    });
 
